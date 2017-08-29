77ccdf27c04a7ccdb47b1e5dc48249c2f11da5ea
Anusha Rao <anusha2701@gmail.com>
2017-08-29 23:34:18 +0200

    Added internet testing with Xprivacy

--- app/src/main/java/anusha/test/xprivaxy/de/xprivacytest/MainActivity.java
+++ app/src/main/java/anusha/test/xprivaxy/de/xprivacytest/MainActivity.java
@@ -4,28 +4,66 @@ import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView imeiNumber;
+    Button httpCallButton;
+    TextView httpResponseHolder;
     private static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 0;
+    private final OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imeiNumber = (TextView) findViewById(R.id.imei_holder);
-
+        init();
+        initListeners();
         loadImei();
+    }
 
+    private void initListeners() {
+        httpCallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                if(isNetworkConnected()){
+                    Toast.makeText(getApplicationContext(), "Internet connected", Toast.LENGTH_SHORT).show();
+                    //make http call
+                    new OkHttpHandler().execute();
+                }
+            }
+        });
+    }
+
+    private void init() {
+        imeiNumber = (TextView) findViewById(R.id.imei_holder);
+        httpCallButton = (Button) findViewById(R.id.http_call_button);
+        httpResponseHolder = (TextView) findViewById(R.id.http_response_holder);
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null;
     }
 
     private void loadImei() {
@@ -87,5 +125,38 @@ public class MainActivity extends AppCompatActivity {
                     }
                 })
                 .show();
+    }
+
+    private class OkHttpHandler extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            String responseString = "";
+            Request request = new Request.Builder()
+                    .url("http://publicobject.com/helloworld.txt")
+                    .build();
+
+            try{
+                Response response = client.newCall(request).execute();
+                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                Headers responseHeaders = response.headers();
+                for (int i = 0; i < responseHeaders.size(); i++) {
+                    System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                }
+                responseString = response.body().string();
+                System.out.println(responseString);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            httpResponseHolder.setText(s);
+        }
     }
 }
